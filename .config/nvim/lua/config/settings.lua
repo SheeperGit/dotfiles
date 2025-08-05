7e32ed653f4d2579be0e257d3de85284b6d3f17a
SheeperGit <sean1900.s@gmail.com>
2025-08-04 23:18:17 -0400

    nvim: remove expandtab

--- .config/nvim/lua/config/settings.lua
+++ .config/nvim/lua/config/settings.lua
@@ -24,7 +24,6 @@ vim.opt.mouse = 'a'                                -- Enable mouse support
 vim.opt.tabstop = 2                                -- Tab width
 vim.opt.shiftwidth = 2                             -- Indent width
 vim.opt.softtabstop = 2                            -- Soft tab stop
-vim.opt.expandtab = true                           -- Use spaces instead of tabs
 vim.opt.smartindent = true                         -- Smart auto-indenting
 vim.opt.autoindent = true                          -- Copy indent from current line
 
