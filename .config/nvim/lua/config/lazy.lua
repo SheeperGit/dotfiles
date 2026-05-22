a5fd0545032694634a1759296cc085adde3a03fe
SheeperGit <sean1900.s@gmail.com>
2026-05-22 12:22:06 -0400

    nvim: disable luarocks

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -26,6 +26,10 @@ require("lazy").setup({
     -- import your plugins
     { import = "plugins" },
   },
+	-- disable luarocks support
+	rocks = {
+    enabled = false,
+  },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
   install = { 
